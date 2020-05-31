c2f86c0815acdcb2b3eb1f76f8487473ae01f610
Soul-Childe <1067783703@qq.com>
2020-05-31 21:24:08 +0800

    Soul-Childe

--- System.BLL/bll.cs
+++ System.BLL/bll.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace System.BLL
 {
-    public class Class1
+    public class bll
     {
     }
 }
--- System.DAL/dal.cs
+++ System.DAL/dal.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace System.DAL
 {
-    public class Class1
+    public class dal
     {
     }
 }
